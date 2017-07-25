5d3906e99bcaaae8a05b4b5b200400589b58c3b7
Anu <anuththari@gmail.com>
2017-07-25 14:21:40 -0400

    Set up DSD test file with DSD source code + Spectral Clustering

---
+++ Python/DSD/dsd_test.py
@@ -0,0 +1,34 @@
+# Main DSD clustering test file
+#
+# Uses DSD to generate a new graph from input adjacency matrix and 
+# uses spectral clustering to identify clusters.
+#
+# 7/25/2017, Anuththari Gamage
+#
+
+import numpy as np
+import sklearn.cluster as sc
+
+# load DSD matrix from file
+data = np.loadtxt('outputfile.DSD1', skiprows=1, usecols=(np.linspace(1,10,10,
+    dtype=int)));
+vertices = np.loadtxt('outputfile.DSD1', skiprows=1, usecols=(0), dtype=str)
+sorted_idx = np.argsort(vertices) # obtain correct indices  since vertices 
+                                  # are out of order in the DSD matrix
+    
+# compute Gaussian kernel     
+sigma = 5 
+data = np.exp(-data**2 / (2.*(sigma**2)))
+
+# apply spectral clustering and reorder labels
+spectral = sc.SpectralClustering(n_clusters = 3, affinity='precomputed')
+spectral.fit(data)
+labels = spectral.fit_predict(data) 
+true_labels = np.zeros(len(labels), dtype=int)  # contains labels in order of
+                                                # vertex (1,...,n)
+for i in range(len(labels)):
+    true_labels[i] = labels[sorted_idx[i]]
+
+print(true_labels)
+
+
