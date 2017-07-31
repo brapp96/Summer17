2e0d857afc536a554492f37dfdde931ec1f5fe07
Brian <emurox@yahoo.com>
2017-07-31 11:02:03 -0400

    combining various versions pt 1

--- MATLAB/NodeEmbed/testfile.m
+++ MATLAB/NodeEmbed/testfile.m
@@ -1,35 +1,26 @@
-N = 2000;
+N = 100;%[100,200,500,1000,2000,5000,10000];
 c = [2,3,4,5,6,8,10,12,15,20];
-iter = 0:4;
-num_reps = 5;
-num_total = num_reps*(max(iter)+1);
+iters = 0:4;
+num_reps = 2;
+num_total = num_reps*(max(iters)+1);
 
-cbt = zeros(num_total,numel(c));
-cnbt = zeros(num_total,numel(c));
-nbt = zeros(num_total,numel(c));
-nnbt = zeros(num_total,numel(c));
-for i = 1:numel(c)
-    for iter = 0:4
-        [G,L] = import_graph_by_edges(N,2,c(i),.9,iter);
-        for rep = 1:num_reps
-            fprintf('c = %d, iter %d, rep %d\n',c(i),iter,rep);
-            ind = num_reps*(iter)+rep;
-            tic
-            [~,cbt(ind,i),nbt(ind,i)] = node_embed_file(G,L,0);
-            toc
-            [~,cnbt(ind,i),nnbt(ind,i)] = node_embed_file(G,L,1);
-            toc
+ccr_bt = zeros(numel(N),numel(c),num_total);
+ccr_nbt = ccr_bt;
+nmi_bt = ccr_bt;
+nmi_nbt = ccr_bt;
+logfile = fopen('test.log','wb');
+for i = 1:numel(N)
+    for j = 1:numel(c)
+        for iter = iters
+            [G,L] = import_graph_by_edges(N(i),2,c(j),.9,iter);
+            for rep = 1:num_reps
+                fprintf(logfile,'N = %d, c = %d, iter %d, rep %d\n',N(i),c(j),iter,rep);
+                ind = num_reps*iter+rep;
+                [~,ccr_bt(i,j,ind),nmi_bt(i,j,ind)] = node_embed_file(G,L,0);
+                [~,ccr_nbt(i,j,ind),nmi_nbt(i,j,ind)] = node_embed_file(G,L,1);
+            end
         end
     end
 end
-figure;
-hold on
-yyaxis left
-errorbar(c,mean(nbt),std(nbt))
-errorbar(c,mean(nnbt),std(nnbt))
-yyaxis right
-errorbar(c,mean(cbt),std(cbt))
-errorbar(c,mean(cnbt),std(cnbt))
-legend({'NMI BT','NMI NBT', 'CCR BT', 'CCR NBT'});
-title('Variation of c');
-saveas(gcf,sprintf('N%dvariedc.png',N));
+fclose(logfile);
+save(['figs/nmi_ccr_' datestr(clock,'mm-dd-yy_HH:MM:SS') '.mat'],'nmi_bt','nmi_nbt','ccr_bt','ccr_nbt');
