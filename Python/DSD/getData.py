7149c26629e59d3f582ac7e5dc609e3196b80322
Brian <emurox@yahoo.com>
2017-10-07 12:22:41 -0400

    adding to PPItest.m

--- Python/DSD/getData.py
+++ Python/DSD/getData.py
@@ -6,10 +6,10 @@ import networkx as nx
 filename = 'PPIdata.txt';
 (ppbAdj, names) = PPIparser.GetAdj(filename,0);
 
-nameArray = np.array(names.keys());
-indexArray = np.array(names.values());
+nameArray = np.array(names.keys())
+np.savetxt('PPInames.txt',nameArray,'%s')
 
-graph = nx.from_numpy_matrix(ppbAdj);
+graph = nx.from_numpy_matrix(ppbAdj)
 nx.write_adjlist(graph, 'PPIadj.txt')
 
 #pdb.set_trace()
