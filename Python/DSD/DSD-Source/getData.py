ae9082cd5acafb227866efb37c6cf01b9089b051
Anu <anuththari@gmail.com>
2017-10-07 12:26:07 -0400

    clusters from DSD

--- Python/DSD/DSD-Source/getData.py
+++ Python/DSD/DSD-Source/getData.py
@@ -4,32 +4,46 @@ import pdb
 import networkx as nx
 import sklearn.cluster as sc
  
-filename = 'PPIdata.txt';
-(ppbAdj, names) = PPIparser.GetAdj(filename,0);
-
-nameArray = np.array(names.keys());
-indexArray = np.array(names.values());
-pdb.set_trace()
+#filename = 'PPIdata.txt';
+#(ppbAdj, names) = PPIparser.GetAdj(filename,0);
 #
+#nameArray = np.array(names.keys());
+#indexArray = np.array(names.values());
+#pdb.set_trace()
+
 #graph = nx.from_numpy_matrix(ppbAdj);
 #nx.write_adjlist(graph, 'PPIadj.txt')
-#
-#N = 5885
-#DSD = np.loadtxt('results.DSD1', skiprows=1, usecols=range(1,N));
+
+N = 5885
+DSD = np.loadtxt('results.DSD1', skiprows=1, usecols=range(1,N));
 #pdb.set_trace()
-#DSD_sim = 1/(DSD + np.eye(DSD.shape[0]))
-#
-#            
-## Apply spectral clustering and reorder labels using Hungarian algorithm
-#print('Applying spectral clustering...')
-#labels = sc.spectral_clustering(DSD_sim, n_clusters=2)
+DSD_sim = 1/(DSD + np.eye(DSD.shape[0]))
 #pdb.set_trace()
-##Conf = metrics.confusion_matrix(true_labels, labels)
-##row, col = scipy.optimize.linear_sum_assignment(-1*Conf)
-##
-### Get metrics
-##print('Calculating metrics...')
-##acc_nmi[i] = metrics.normalized_mutual_info_score(true_labels,labels)
-##acc_ccr[i] = float(Conf[row, col].sum())/float(N)
+            
+# Apply spectral clustering and reorder labels using Hungarian algorithm
+print('Applying spectral clustering...')
+labels = sc.spectral_clustering(DSD_sim, n_clusters=43)
+np.savetxt('results_DSD43.txt', labels, fmt='%d')
+
+totals = np.zeros(43)
+for i in range(0,43):
+    totals[i] = np.sum(labels == i)
+
+print(np.sort(totals))
+
+max_size = np.max(totals)
+max_cluster = np.where(totals == max_size)
+
+
+
+pdb.set_trace()
+#pdb.set_trace()
+#Conf = metrics.confusion_matrix(true_labels, labels)
+#row, col = scipy.optimize.linear_sum_assignment(-1*Conf)
+#
+## Get metrics
+#print('Calculating metrics...')
+#acc_nmi[i] = metrics.normalized_mutual_info_score(true_labels,labels)
+#acc_ccr[i] = float(Conf[row, col].sum())/float(N)
             
 
