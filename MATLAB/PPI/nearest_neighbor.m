bde7bb272623957ffe540ed017856820a5bf2484
Brian <emurox@yahoo.com>
2017-10-14 13:25:15 -0400

    Worked out the latex version nonsense

--- MATLAB/PPI/nearest_neighbor.m
+++ MATLAB/PPI/nearest_neighbor.m
@@ -2,6 +2,7 @@ function accuracy = nearest_neighbor(graph,annos,protein_inds,num2remove,t,is_we
     alpha = 3;
     n = numel(protein_inds);
     numCorrect = 0;
+    f1_scores = zeros(4,43); % 1st row: true positives; 2nd row: false positives; 3rd row: false negatives; 4th row: true negatives
     for ii = 1:num2remove
         selectedElement = randi(n,1);
         if is_weighted
@@ -19,7 +20,6 @@ function accuracy = nearest_neighbor(graph,annos,protein_inds,num2remove,t,is_we
             thisVote = annos{minds(jj)};
             votes(thisVote) = votes(thisVote) + invmvals(jj);
         end
-       % votes = hist(vertcat(annos{minds(2:t+1)}),1:43); a little too hacky
         [~,voteinds] = sort(votes);
         max_GOs = voteinds(end:-1:end-(alpha-1));
         orig_GOs = annos{selectedElement};
